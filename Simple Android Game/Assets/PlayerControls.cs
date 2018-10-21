210a2da5bf5990ccd6b2da97796b54ac414c3fd0
Alex Masson <amasson99@gmail.com>
2018-10-21 14:07:28 -0500

    Modified player scripts to include ground collision and double jump checking

--- Simple Android Game/Assets/PlayerControls.cs
+++ Simple Android Game/Assets/PlayerControls.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
-
+public class PlayerControls : MonoBehaviour
+{
     public Rigidbody2D rb;
+    public Transform groundCheck;
+    public Transform startPosition;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
 
-	// Use this for initialization
-	void Start () {
-
+    void Start()
+    {
         rb = GetComponent<Rigidbody2D>();
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
+    void Update()
+    {
         rb.velocity = new Vector2(3, rb.velocity.y);
-
-        if (Input.GetMouseButtonDown(0))
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        if (Input.GetMouseButtonDown(0) && onGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, 5);
         }
 
     }
+
 }
