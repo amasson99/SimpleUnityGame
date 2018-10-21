07be2a7feb1eb6a19e3d6a8d37b21530c965ac46
Alex Masson <amasson99@gmail.com>
2018-10-21 13:52:27 -0500

    Created player controls

---
+++ Simple Android Game/Assets/PlayerControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        rb.velocity = new Vector2(3, rb.velocity.y);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 5);
+        }
+
+    }
+}
